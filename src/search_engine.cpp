ddb8c59a68fef2d76323d3ff8060fcee301a3fbf
BaredJJ <33960321+BaredJJ@users.noreply.github.com>
2021-01-17 13:35:55 +0300

    review commit

--- src/search_engine.cpp
+++ src/search_engine.cpp
@@ -7,6 +7,13 @@
 #include <utility>
 #include <vector>
 
+//1. Поправьте стилистические замечания. Старайтесь не записывать одну логическую еденицу в несколько строк, если это возможно
+//2. Подумайте над функцией DropElementsByValue
+//3. Скомпонуйте объекты в классе
+
+//Работа сделана хорошо. все замечания по стилистике кода. Нужно исправить. Сейчас вы не сдали, но нужно всегда помнить, что чистый код делает
+// его более выразительным, удобочитаемым и лучше сопровождаемым. Жду ваших изменений. Удачи!
+
 using namespace std;
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
@@ -24,8 +31,12 @@ int ReadLineWithNumber() {
     return result;
 }
 
+//Имеет ли смысл создавать функцию, которую вы используете только в одном месте.
+//Так же вы копируете вектор при передаче в функцию, что не есть хорошо.
 template <typename T>
 vector<T> DropElementsByValue(vector<T> v, const T& value) {
+    //Используйте std::remove_if
+    //https://ps-group.github.io/cxx/cxx_remove_if
     v.erase(remove(v.begin(), v.end(), value), v.end());
     return v;
 }
@@ -39,6 +50,7 @@ vector<string> SplitIntoWords(const string& text) {
         }
         if (c == ' ') {
             words.push_back(word);
+            //Используйте метод clear
             word = "";
         } else {
             word += c;
@@ -70,6 +82,16 @@ public:
         }
     }
 
+    //Если вы записываете аргументы в несколько строк, то каждый аргумент должен быть в отдельной строке.
+    //Пример:
+    //void myFunc(
+    //              int first
+    //              ,int second
+    //              ,int third
+    //            )
+    //{
+    //      ...function body...
+    //}
     void AddDocument(int document_id, const string& document,
                      DocumentStatus status, const vector<int>& ratings) {
         const vector<string> words = SplitIntoWordsNoStop(document);
@@ -78,12 +100,14 @@ public:
             word_to_document_freqs_[word][document_id] += inv_word_count;
         }
         documents_.emplace(document_id, 
+            //Лучше второй аргумент записать в одну строку. Лучше будет читаться
             DocumentData{
                 ComputeAverageRating(ratings), 
                 status
             });
     }
 
+    //Те же замечания что и в функции выше
     vector<Document> FindTopDocuments(const string& raw_query,
                                       DocumentStatus status_to_find
                                           = DocumentStatus::ACTUAL) const {
@@ -95,6 +119,8 @@ public:
         );
     }
 
+    //Шаблонные методы должны располагаться в конце блока
+    //Те же замечания по расположению аргументов. Тут лучше записать в одну строку
     template <typename KeyMapper>
     vector<Document> FindTopDocuments(const string& raw_query,
                                       KeyMapper key_mapper) const {
@@ -103,6 +129,8 @@ public:
 
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
+                //Попробуйте использовать тернарный опреатор
+                //https://ravesli.com/urok-41-sizeof-zapyataya-i-uslovnyj-ternarnyj-operator/
                 if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
                     return lhs.rating > rhs.rating;
                 } else {
@@ -128,9 +156,13 @@ public:
             }
         }
         for (const string& word : query.minus_words) {
+            //Данное условие можно вынести в функцию, так как используется несколько раз
+            //Что ниубдь IsContainWord
             if (word_to_document_freqs_.count(word) == 0) {
                 continue;
             }
+            //Данное условие можно вынести в функцию, так как используется несколько раз
+            //Что ниубдь IsWordContainId
             if (word_to_document_freqs_.at(word).count(document_id)) {
                 matched_words.clear();
                 break;
@@ -150,6 +182,8 @@ private:
     map<int, DocumentData> documents_;
 
     bool IsStopWord(const string& word) const {
+        //Это исключительно для вас.
+        //В данном случае нет необходимости сравнивать резуьтат с нулем, т.к. в C++ true != 0, а false = 0
         return stop_words_.count(word) > 0;
     }
 
@@ -168,9 +202,11 @@ private:
         for (const int rating : ratings) {
             rating_sum += rating;
         }
+        //Что будет если коллекция будет пустая?
         return rating_sum / static_cast<int>(ratings.size());
     }
 
+    //Сгруппируйте структуры в одном месте
     struct QueryWord {
         string data;
         bool is_minus;
@@ -199,6 +235,7 @@ private:
         Query query;
         for (const string& word : SplitIntoWords(text)) {
             const QueryWord query_word = ParseQueryWord(word);
+            // Можно сделать инверсию условия и избавиться от двойной вложенности
             if (!query_word.is_stop) {
                 if (query_word.is_minus) {
                     query.minus_words.insert(query_word.data);
@@ -224,13 +261,17 @@ private:
             if (word_to_document_freqs_.count(word) == 0) {
                 continue;
             }
+            //Лучше записать в одну строку
             const double inverse_document_freq
                 = ComputeWordInverseDocumentFreq(word);
+            //Лучше записать в одну строку
             for (const auto [document_id, term_freq]
                     : word_to_document_freqs_.at(word)) {
+                //Закрывающую скобку на отдельную строку и открывающую кобку условия, тоже на отдельную строку
                 if (key_mapper(document_id,
                                documents_.at(document_id).status,
                                documents_.at(document_id).rating)) {
+                    //Лучше записать в одну строку
                     document_to_relevance[document_id]
                         += term_freq * inverse_document_freq;
                 }
@@ -241,6 +282,7 @@ private:
             if (word_to_document_freqs_.count(word) == 0) {
                 continue;
             }
+            //Лучше записать в одну строку
             for (const auto [document_id, _]
                     : word_to_document_freqs_.at(word)) {
                 document_to_relevance.erase(document_id);
@@ -300,6 +342,8 @@ int main() {
     }
 
     cout << "Even ids:"s << endl;
+    //Написано красиво, но читать очень сложно. Где тут лямбда, где условие, где тело.
+    //Лучше вынесите лямбду в переменную, это не критично и код станет понятней.
     for (const Document& document
             : search_server.FindTopDocuments(
                 "пушистый ухоженный кот"s,
@@ -312,6 +356,7 @@ int main() {
     }
 
     cout << "Positive rating only:"s << endl;
+    //Тоже что и выше
     for (const Document& document
             : search_server.FindTopDocuments(
                 "пушистый  ухоженный кот"s,
