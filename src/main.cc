e49586c1da60b525650f75f8a91ab71d35d14033
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2021-02-28 20:36:13 +0300

    Rename search_engine.cpp to main.cc

--- src/main.cc
+++ src/main.cc
@@ -533,23 +533,23 @@ ostream& operator<<(ostream& out, const vector<Document>& documents) {
 /* ------------------------------------------------------------------------- */
 
 int main() {
-    SearchServer search_server("and at on in"s);
-    RequestQueue request_queue(search_server);
+    SearchServer search_server("and with"s);
 
-    search_server.AddDocument(1, "curly cat curly tail"s, DocumentStatus::ACTUAL, {7, 2, 7});
-    search_server.AddDocument(2, "curly dog and fancy collar"s, DocumentStatus::ACTUAL, {1, 2, 3});
-    search_server.AddDocument(3, "big cat fancy collar "s, DocumentStatus::ACTUAL, {1, 2, 8});
-    search_server.AddDocument(4, "big dog sparrow Eugene"s, DocumentStatus::ACTUAL, {1, 3, 2});
-    search_server.AddDocument(5, "big dog sparrow Vasiliy"s, DocumentStatus::ACTUAL, {1, 1, 1});
+    search_server.AddDocument(1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "funny pet with curly hair"s, DocumentStatus::ACTUAL, {4, 2, 3});
+    search_server.AddDocument(3, "big cat nasty hair"s, DocumentStatus::ACTUAL, {1, 2, 8});
+    search_server.AddDocument(4, "big dog and rat Vladimir"s, DocumentStatus::ACTUAL, {1, -3, 2});
+    search_server.AddDocument(5, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
+    search_server.AddDocument(6, "small dog Jack-Russell terier"s, DocumentStatus::ACTUAL, {5, 3, 4});
+    search_server.AddDocument(7, "dog Siberian hasky"s, DocumentStatus::ACTUAL, {2, 1, 3});
 
-    for (int i = 0; i < 1439; ++i) {
-        request_queue.AddFindRequest("empty request"s);
+    const vector<Document> search_results = search_server.FindTopDocuments("curly dog"s);
+    const auto pages = Paginate(search_results, 2);
+
+    for (auto page = pages.begin(); page != pages.end(); ++page) {
+        cout << *page << endl;
+        cout << "Page break"s << endl;
     }
-    request_queue.AddFindRequest("curly dog"s);
-    request_queue.AddFindRequest("big collar"s);
-    request_queue.AddFindRequest("sparrow"s);
-
-    cout << "Total empty requests: "s << request_queue.GetNoResultRequests() << endl;
 
     return 0;
 }
