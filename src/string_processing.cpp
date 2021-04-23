745ebf38eb7e695147449b1a4729c813b4c89559
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2021-04-24 00:16:32 +0300

    Use std::for_each() in SplitIntoWords()

--- src/string_processing.cpp
+++ src/string_processing.cpp
@@ -1,25 +1,21 @@
 #include "string_processing.h"
 
 std::vector<std::string> SplitIntoWords(const std::string& text) {
-    std::vector<std::string> words;
-    std::string word;
-    for (const char c : text) {
-        if (text.size() == 0)
-            break;
-
-        if (c == ' ') {
-            words.push_back(word);
-            word.clear();
-        } else {
-            word += c;
+    std::vector<std::string> words{""};
+    std::for_each(
+        text.begin(), text.end(),
+        [&words](const char& c) {
+            if (c != ' ')
+                words.back().push_back(c);
+            else if (!words.back().empty())
+                words.push_back({});
         }
-    }
-
-    words.push_back(word);
-    words.erase(
-        remove_if(words.begin(), words.end(),
-                  [](const std::string& word) { return word.empty(); }),
-        words.end()
     );
+
+    // The last element of words vector will be empty
+    // if a text at its end contains spaces
+    if (words.back().empty())
+        words.pop_back();
+
     return words;
 }
